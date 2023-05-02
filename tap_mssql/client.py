420a21d0e48d16b5cdeb2987d0d9783f36d91ce5
Dan Norman <buzzcutnorman@gmail.com>
2023-05-02 13:42:53 -0700

    comment out loggers

--- tap_mssql/client.py
+++ tap_mssql/client.py
@@ -506,11 +506,15 @@ class mssqlStream(SQLStream):
         if self.replication_key:
             replication_key_col = table.columns[self.replication_key]
             query = query.order_by(replication_key_col)
-            # remove all below in final #
-            self.logger.info(f"The replication_key_col SQLA type: {replication_key_col.type}")
-            self.logger.info(f"The replication_key_col python type: {replication_key_col.type.python_type} this is type {type(replication_key_col.type.python_type)}")
-            self.logger.info(f"Is the a replication_key_col python type datetime or date: {(replication_key_col.type.python_type in (datetime.datetime, datetime.date))}")
-            # remove all to here in final #
+            # # remove all below in final #
+            # self.logger.info('\n')
+            # self.logger.info(f"The replication_key_col SQLA type: {replication_key_col.type}")
+            # self.logger.info(' ')
+            # self.logger.info(f"The replication_key_col python type: {replication_key_col.type.python_type} this is type {type(replication_key_col.type.python_type)}")
+            # self.logger.info(' ')
+            # self.logger.info(f"Is the a replication_key_col python type datetime or date: {(replication_key_col.type.python_type in (datetime.datetime, datetime.date))}")
+            # self.logger.info('\n')
+            # # remove all to here in final #
             if replication_key_col.type.python_type in (datetime.datetime, datetime.date):
                 start_val = self.get_starting_timestamp(context)
             else:
@@ -531,30 +535,28 @@ class mssqlStream(SQLStream):
             # processed.
             query = query.limit(self.ABORT_AT_RECORD_COUNT + 1)
 
-        # remove all below in final #
-        self.logger.info('\n')
-        self.logger.info(f"Passed context is: {self.schema}")
-        self.logger.info('\n')
-        self.logger.info(f"Passed context is: {context}")
-        self.logger.info(' ')
-        self.logger.info(f"tap_state is: {self.tap_state}")
-        self.logger.info(' ')
-        self.logger.info(f"stream_state is: {self.stream_state}")
-        self.logger.info(' ')
-        self.logger.info(f"get_context_state is: {self.get_context_state(context)}")
-        self.logger.info(' ')
-        self.logger.info(f"replication_key is type : {type(self.replication_key)} has value: {self.replication_key}")
-        self.logger.info(' ')
-        if self.replication_key:
-            self.logger.info(f"replication_key_col type: {type(replication_key_col)}, replication_key_col type: {type(replication_key_col)}")
-            self.logger.info(' ')
-            self.logger.info(f"get_starting_replication_key_value is: {self.get_starting_replication_key_value(context)}")
-            self.logger.info(' ')
-            self.logger.info(f"start_val type: {type(start_val)}, start_val type: {type(start_val)}")
-            self.logger.info(' ')
-        self.logger.info(query)
-        self.logger.info('\n')
-        # remove all to here in final #
+        # # remove all below in final #
+        # self.logger.info('\n')
+        # self.logger.info(f"Passed context is: {context}")
+        # self.logger.info(' ')
+        # self.logger.info(f"tap_state is: {self.tap_state}")
+        # self.logger.info(' ')
+        # self.logger.info(f"stream_state is: {self.stream_state}")
+        # self.logger.info(' ')
+        # self.logger.info(f"get_context_state is: {self.get_context_state(context)}")
+        # self.logger.info(' ')
+        # self.logger.info(f"replication_key is type : {type(self.replication_key)} has value: {self.replication_key}")
+        # self.logger.info(' ')
+        # if self.replication_key:
+        #     self.logger.info(f"replication_key_col type: {type(replication_key_col)}, replication_key_col type: {type(replication_key_col)}")
+        #     self.logger.info(' ')
+        #     self.logger.info(f"get_starting_replication_key_value is: {self.get_starting_replication_key_value(context)}")
+        #     self.logger.info(' ')
+        #     self.logger.info(f"start_val type: {type(start_val)}, start_val type: {type(start_val)}")
+        #     self.logger.info(' ')
+        # self.logger.info(query)
+        # self.logger.info('\n')
+        # # remove all to here in final #
 
         with self.connector._connect() as conn:
             for record in conn.execute(query):
