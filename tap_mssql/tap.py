494073d008e3180669b2e691426400ac39ce82bc
Dan Norman <buzzcutnorman@gmail.com>
2023-08-08 09:26:17 -0600

    fix: Designated required config fields (#64)
    * added required=True to certain fields
    
    * updated settings to match required fields
    
    * removed unused imports
    
    * added defaults and allowed values
    
    * removed unnecessary config value checking
    
    * updated config options default info

--- tap_mssql/tap.py
+++ tap_mssql/tap.py
@@ -52,17 +52,24 @@ class Tapmssql(SQLTap):
         th.Property(
             "dialect",
             th.StringType,
-            description="The Dialect of SQLAlchamey"
+            description="The Dialect of SQLAlchamey",
+            required=True,
+            allowed_values=["mssql"],
+            default="mssql"
         ),
         th.Property(
             "driver_type",
             th.StringType,
-            description="The Python Driver you will be using to connect to the SQL server"
+            description="The Python Driver you will be using to connect to the SQL server",
+            required=True,
+            allowed_values=["pyodbc", "pymssql"],
+            default="pymssql"
         ),
         th.Property(
             "host",
             th.StringType,
-            description="The FQDN of the Host serving out the SQL Instance"
+            description="The FQDN of the Host serving out the SQL Instance",
+            required=True
         ),
         th.Property(
             "port",
@@ -72,17 +79,21 @@ class Tapmssql(SQLTap):
         th.Property(
             "user",
             th.StringType,
-            description="The User Account who has been granted access to the SQL Server"
+            description="The User Account who has been granted access to the SQL Server",
+            required=True
         ),
         th.Property(
             "password",
             th.StringType,
-            description="The Password for the User account"
+            description="The Password for the User account",
+            required=True,
+            secret=True
         ),
         th.Property(
             "database",
             th.StringType,
-            description="The Default database for this connection"
+            description="The Default database for this connection",
+            required=True
         ),
         th.Property(
             "sqlalchemy_eng_params",
