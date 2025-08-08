f08a0c2dd9aae217f6af1aa76729f63659067526
BuzzCutNorman <buzzcutnorman@gmail.com>
2025-08-08 14:31:15 -0600

    forgot to comment out something

--- tap_mssql/client.py
+++ tap_mssql/client.py
@@ -68,7 +68,7 @@ class MSSQLConnector(SQLConnector):
             The URL as a string.
         """
         if config.get("hard_coded_sqla_url"):
-            self.logger.info(f"This is the url: {config.get("hard_coded_sqla_url")}")
+            # self.logger.info(f"This is the url: {config.get('hard_coded_sqla_url')}")
             return config.get("hard_coded_sqla_url")
 
         url_drivername = f"{config.get('dialect')}+{config.get('driver_type')}"
